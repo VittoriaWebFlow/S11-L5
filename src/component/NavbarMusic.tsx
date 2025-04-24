373c1051a1cf28b56d345d467db22e744e954103
Vittoria Sacchi <vittoriasacchi7@gmail.com>
2025-04-24 12:01:18 +0200

    Update NavbarMusic.tsx

--- src/component/NavbarMusic.tsx
+++ src/component/NavbarMusic.tsx
@@ -1,44 +1,55 @@
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Button, Dropdown } from 'react-bootstrap';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function NavbarMusic() {
   return (
-    <Container fluid={true} className='p-0 '>
-      <Row>
-        <Col >
-          <Navbar bg="dark" data-bs-theme="dark">
-           
-              <div>
-                <i className="bi bi-shuffle me-2 text-secondary-emphasis"></i>
-                <i className="bi bi-rewind text-secondary-emphasis me-2"></i>
-                <i className="bi bi-play text-secondary-emphasis me-2"></i>
-                <i className="bi bi-fast-forward text-secondary-emphasis me-2"></i>
-                <i className="bi bi-repeat text-secondary-emphasis me-2"></i>
-              </div>
-              <section className="header-section ">
-              <i className="bi bi-apple text-light "></i>
-                  </section>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                  <div>
-                  <i className="bi bi-volume-up-fill text-secondary-emphasis fs-4" ></i>
-                  </div>
-                  
-                 <Button className='me-4 bg-danger border-0 '><i className="bi bi-person-fill bg-none fs-5"></i> Accedi</Button>
-                  
-                </Nav>
-              </Navbar.Collapse>
-            
-          </Navbar>
-        </Col>
-      </Row>
-    </Container>
+    <Navbar bg="black" variant="dark" expand="lg" className="px-3 py-2">
+      <Container fluid className="d-flex justify-content-between align-items-center flex-wrap">
+        
+       
+        <div className="d-flex align-items-center gap-3 order-1">
+          <i className="bi bi-shuffle text-secondary fs-5 d-none d-md-block"></i>
+          <i className="bi bi-rewind text-secondary fs-5 d-none d-md-block"></i>
+          <i className="bi bi-play text-secondary fs-5 d-none d-md-block"></i>
+          <i className="bi bi-fast-forward text-secondary fs-5 d-none d-md-block"></i>
+          <i className="bi bi-repeat text-secondary fs-5 d-none d-md-block"></i>
+
+         
+          <Dropdown className="d-block d-md-none ">
+            <Dropdown.Toggle variant="danger" size="sm" >
+              ☰
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Home</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Novità</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Radio</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+
+        
+        <div className="order-2 ms-auto header-section text-center d-none d-md-block">
+          <i className="bi bi-apple text-light fs-4"></i>
+        </div>
+
+     
+        <div className="order-2 ms-auto text-center d-flex d-md-none">
+          <i className="bi bi-apple text-light fs-4">Music</i>
+        </div>
+
+       
+        <div className="d-flex align-items-center gap-3 order-3 ms-auto">
+          <p className='text-danger mb-0 d-flex d-lg-none'>Accedi</p>
+          <i className="bi bi-volume-up-fill text-secondary fs-4 d-none d-md-block"></i>
+          <Button className="bg-danger border-0 d-flex align-items-center gap-2 px-3 d-none d-lg-block">
+            <i className="bi bi-person-fill fs-5"></i> <span className="d-none d-sm-inline"><strong>Accedi</strong></span>
+          </Button>
+        </div>
+      </Container>
+    </Navbar>
   );
 }
 
